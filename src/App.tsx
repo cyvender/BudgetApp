4b90f7ad1ea18b9c0f8ac34d9acd67c144d3057e
IsraelCarlebach <name@domain.example>
2023-08-03 18:13:07 -0400

    Newest addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { SetStateAction, useState } from "react";
 import './App.css'
+import './Test.tsx'
+import Test from "./Test.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [income, setIncome] = useState('');
+  const [incomet, setIncomet] = useState('');
+  const[list, setList] = useState<{ income: string; incomet: string }[]>([]);
+
+  const handleSubmit=(e:any) =>{
+    e.preventDefault();
+    const data = {
+      income: income,
+      incomet: incomet
+    }
+    if(income && incomet) {
+      setList([...list,data])
+      setIncome("")
+      setIncomet("")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <h1>Budgeting App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Income Type"
+          value={incomet}
+          onChange={(e) => setIncomet(e.target.value)}
+        />
+        <input
+          placeholder="Income"
+          value={income}
+          onChange={(e) => setIncome(e.target.value)}
+        />
+        <button type="submit">Enter</button>
+      </form>
 
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item.incomet} - {item.income}</li>
+        ))}
+      </ul>
+      <Test />
+    </div>
+  );
+  }
 export default App
---
+++ src/Test.tsx
@@ -0,0 +1,7 @@
+function Test () {
+    return (
+        <p>Testing</p>
+    )
+}
+
+export default Test
